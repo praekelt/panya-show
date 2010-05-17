828b468f2fd5ba77a8fe89aa2bfe8524560b6033
shaunsephton <shaunsephton@gmail.com>
2010-05-17 07:23:05 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-show',
+    version='dev',
+    description='Django show app.',
+    author='Praekelt Consulting',
+    author_email='dev@praekelt.com',
+    url='http://github.com/praekelt/django-show',
+    packages = find_packages(),
+    include_package_data=True,
+)
