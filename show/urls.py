d7d8a39e4a39215f578b9183b36971567b58ac5e
shaunsephton <shaunsephton@gmail.com>
2010-05-19 14:58:46 +0200

    added detail view

--- show/urls.py
+++ show/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns(
     'show.views',
     url(r'^radioshow/entrylist/$', 'radioshow_entryitem_list', name='radioshow_entryitem_list'),
+    url(r'^showcontributor/(?P<slug>[\w-]+)/$', 'showcontributor_detail', name='showcontributor_detail'),
 )
--- show/views.py
+++ show/views.py
@@ -1,7 +1,16 @@
 from cal.models import EntryItem
 from cal.pagemenus import EntriesByWeekdaysPageMenu
-from content.generic.views import GenericObjectList 
-from show.models import RadioShow
+from content.generic.views import GenericObjectList
+from content.models import ModelBase
+from show.models import RadioShow, ShowContributor
+
+class RadioShowContributorURL(object):
+    def __call__(self, obj):
+        contributors = obj.get_primary_contributors()
+        if contributors:
+            return contributors[0].get_absolute_url()
+        else:
+            ''
 
 class RadioShowEntryItemList(GenericObjectList):
     def get_extra_context(self, *args, **kwargs):
@@ -24,5 +33,43 @@ class RadioShowEntryItemList(GenericObjectList):
     
     def get_queryset(self):
         return EntryItem.permitted.by_model(RadioShow).order_by('start')
+    
+    def get_url_callable(self):
+        return RadioShowContributorURL()
 
 radioshow_entryitem_list = RadioShowEntryItemList()
+
+class ShowContributorDetail(GenericObjectList):
+    def get_extra_context(self, slug, *args, **kwargs):
+        extra_context = super(ShowContributorDetail, self).get_extra_context(*args, **kwargs)
+        added_context = {
+            'title': 'DJS & Shows',
+            'contributor': ShowContributor.permitted.get(slug=slug)
+        }
+        if extra_context:
+            extra_context.update(
+                added_context,
+            )
+        else:
+            extra_context = added_context
+
+        return extra_context
+   
+    def get_template_name(self):
+        return 'show/showcontributor_detail.html'
+    
+    def get_pagemenu(self, request, queryset):
+        return None
+        return  EntriesByWeekdaysPageMenu(queryset=queryset, request=request)
+
+    def get_queryset(self, slug):
+        owner = ShowContributor.permitted.get(slug=slug).owner
+        if owner:
+            return ModelBase.permitted.filter(owner=owner).order_by('created')
+        else:
+            return ModelBase.permitted.exclude(owner=owner)
+    
+    def get_url_callable(self):
+        return RadioShowContributorURL()
+
+showcontributor_detail = ShowContributorDetail()
