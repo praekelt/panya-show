84af790090b413331fdaba5adc01c8926e240c72
shaunsephton <shaunsephton@gmail.com>
2010-05-19 14:59:32 +0200

    pass through context

--- show/templatetags/show_inclusion_tags.py
+++ show/templatetags/show_inclusion_tags.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('show/inclusion_tags/radioshow_entryitem_listing.html')
-def radioshow_entryitem_listing(object_list):
-    return {'object_list': object_list}
+@register.inclusion_tag('show/inclusion_tags/radioshow_entryitem_listing.html', takes_context=True)
+def radioshow_entryitem_listing(context, object_list):
+    context.update({'object_list': object_list})
+    return context
