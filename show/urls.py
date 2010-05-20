cac4e3cffaa3b56b9792eb60ece12880bb7bfa36
shaunsephton <shaunsephton@gmail.com>
2010-05-20 12:44:00 +0200

    added list url

--- show/urls.py
+++ show/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns(
     'show.views',
     url(r'^radioshow/entrylist/$', 'radioshow_entryitem_list', name='radioshow_entryitem_list'),
-    url(r'^showcontributor/contentlist/(?P<slug>[\w-]+)/$', 'showcontributor_content_list', name='showcontributor_content_list'),
+    url(r'^showcontributor/list/(?P<slug>[\w-]+)/$', 'showcontributor_content_list', name='showcontributor_content_list'),
     url(r'^showcontributor/(?P<slug>[\w-]+)/$', 'showcontributor_detail', name='showcontributor_detail'),
     url(r'^showcontributor/content/(?P<slug>[\w-]+)/$', 'showcontributor_content_detail', name='showcontributor_content_detail'),
 )
