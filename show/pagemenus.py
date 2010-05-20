3ab9bc1933579cc4c5fc5154f7153b5203f62e4a
shaunsephton <shaunsephton@gmail.com>
2010-05-20 09:51:39 +0200

    first commit

---
+++ show/pagemenus.py
@@ -0,0 +1,13 @@
+from pagemenu.pagemenus import PageMenu
+from pagemenu.items import URLPatternItem
+from django.core.urlresolvers import reverse
+
+class ShowContributorPageMenu(PageMenu):
+    def __init__(self, queryset, request, slug, *args, **kwargs):
+        self.items = [
+            URLPatternItem(request, title="Blog", path=reverse('showcontributor_content_list', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_content_list',], default=False),
+            URLPatternItem(request, title="Profile", path=reverse('showcontributor_detail', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_detail',], default=False),
+            #URLPatternItem(request, title="Contact", path=reverse('showcontributor_detail', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_detail',], default=False),
+            #URLPatternItem(request, title="Appearances", path=reverse('showcontributor_detail', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_detail',], default=False),
+        ]
+        super(ShowContributorPageMenu, self).__init__(queryset, request)
