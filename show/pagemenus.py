a62da33bbb88ce0063dc04693bdb04c9af8b4291
shaunsephton <shaunsephton@gmail.com>
2010-05-20 12:44:43 +0200

    correct items

--- show/pagemenus.py
+++ show/pagemenus.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse
 class ShowContributorPageMenu(PageMenu):
     def __init__(self, queryset, request, slug, *args, **kwargs):
         self.items = [
-            URLPatternItem(request, title="Blog", path=reverse('showcontributor_content_list', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_content_list',], default=False),
+            URLPatternItem(request, title="Blog", path=reverse('showcontributor_content_list', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_content_list', 'showcontributor_content_detail',], default=False),
             URLPatternItem(request, title="Profile", path=reverse('showcontributor_detail', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_detail',], default=False),
             #URLPatternItem(request, title="Contact", path=reverse('showcontributor_detail', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_detail',], default=False),
             #URLPatternItem(request, title="Appearances", path=reverse('showcontributor_detail', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_detail',], default=False),
