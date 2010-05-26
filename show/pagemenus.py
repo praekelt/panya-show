80dc734f1e30f6ee3c5bc3d2b6ca1d6b2091413c
jonathan <jonathan@qoda-server.(none)>
2010-05-26 14:48:30 +0200

    added appearances

--- show/pagemenus.py
+++ show/pagemenus.py
@@ -8,6 +8,6 @@ class ShowContributorPageMenu(PageMenu):
             URLPatternItem(request, title="Blog", path=reverse('showcontributor_content_list', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_content_list', 'showcontributor_content_detail',], default=False),
             URLPatternItem(request, title="Profile", path=reverse('showcontributor_detail', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_detail',], default=False),
             #URLPatternItem(request, title="Contact", path=reverse('showcontributor_detail', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_detail',], default=False),
-            #URLPatternItem(request, title="Appearances", path=reverse('showcontributor_detail', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_detail',], default=False),
+            URLPatternItem(request, title="Appearances", path=reverse('showcontributor_appearance_list', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_appearance_list',], default=False),
         ]
         super(ShowContributorPageMenu, self).__init__(queryset, request)
