00e83b0189fa2a811552d352c68055553b0b1999
shaunsephton <shaunsephton@gmail.com>
2010-06-24 09:00:53 +0200

    use new gen views

--- show/views.py
+++ show/views.py
@@ -1,70 +1,57 @@
 from django.core.urlresolvers import reverse
 
 from cal.models import EntryItem
-from cal.pagemenus import EntriesByWeekdaysPageMenu
+from cal.view_modifiers import EntriesByWeekdaysViewModifier
 from panya.generic.views import GenericObjectDetail, GenericObjectList
 from panya.models import ModelBase
 from event.models import Event
 from show.models import Appearance, RadioShow, ShowContributor
-from show.pagemenus import ShowContributorPageMenu
+from show.view_modifiers import ShowContributorViewModifier
 
 class ShowContributerContentURL(object):
-    def __call__(self, obj):
-        return reverse('showcontributor_content_detail', kwargs={'slug': obj.slug})
+    def __call__(self, obj=None):
+        if obj:
+            return reverse('showcontributor_content_detail', kwargs={'slug': obj.slug})
+        else:
+            return self
 
 class RadioShowEntryItemList(GenericObjectList):
     def get_extra_context(self, *args, **kwargs):
-        extra_context = super(RadioShowEntryItemList, self).get_extra_context(*args, **kwargs)
-        added_context = {'title': 'DJS & Shows'}
-        if extra_context:
-            extra_context.update(
-                added_context,
-            )
-        else:
-            extra_context = added_context
-
-        return extra_context
+        return  {'title': 'DJS & Shows'}
     
-    def get_template_name(self):
+    def get_template_name(self, *args, **kwargs):
         return 'show/radioshow_entryitem_list.html'
     
-    def get_pagemenu(self, request, queryset, *args, **kwargs):
-        return  EntriesByWeekdaysPageMenu(queryset=queryset, request=request)
+    def get_view_modifier(self, request, *args, **kwargs):
+        return  EntriesByWeekdaysViewModifier(request=request, *args, **kwargs)
     
-    def get_queryset(self):
+    def get_queryset(self, *args, **kwargs):
         return EntryItem.permitted.by_model(RadioShow).order_by('start')
     
 radioshow_entryitem_list = RadioShowEntryItemList()
 
 class ShowContributorContentList(GenericObjectList):
-    def get_extra_context(self, slug, *args, **kwargs):
-        extra_context = super(ShowContributorContentList, self).get_extra_context(*args, **kwargs)
-        added_context = {
+    def get_extra_context(self, *args, **kwargs):
+        slug = kwargs['slug']
+        return {
             'title': 'DJS & Shows',
             'contributor': ShowContributor.permitted.get(slug=slug)
         }
-        if extra_context:
-            extra_context.update(
-                added_context,
-            )
-        else:
-            extra_context = added_context
-
-        return extra_context
    
-    def get_template_name(self):
+    def get_template_name(self, *args, **kwargs):
         return 'show/showcontributor_content_list.html'
     
-    def get_url_callable(self):
+    def get_url_callable(self, *args, **kwargs):
         return ShowContributerContentURL()
     
-    def get_pagemenu(self, request, queryset, slug, *args, **kwargs):
-        return ShowContributorPageMenu(queryset=queryset, request=request, slug=slug)
+    def get_view_modifier(self, request, slug, *args, **kwargs):
+        return ShowContributorViewModifier(request=request, slug=slug, *args, **kwargs)
     
-    def get_paginate_by(self):
+    def get_paginate_by(self, *args, **kwargs):
         return 7
 
-    def get_queryset(self, slug):
+    def get_queryset(self, *args, **kwargs):
+        slug = kwargs['slug']
         owner = ShowContributor.permitted.get(slug=slug).owner
         if owner:
             return ModelBase.permitted.filter(owner=owner).exclude(class_name__in=["ShowContributor",]).order_by('created')
@@ -74,76 +61,61 @@ class ShowContributorContentList(GenericObjectList):
 showcontributor_content_list = ShowContributorContentList()
 
 class ShowContributorDetail(GenericObjectDetail):
-    def get_template_name(self):
+    def get_template_name(self, *args, **kwargs):
         return 'show/showcontributor_detail.html'
     
-    def get_pagemenu(self, request, queryset, slug, *args, **kwargs):
-        return ShowContributorPageMenu(queryset=queryset, request=request, slug=slug)
+    def get_view_modifier(self, request, slug, *args, **kwargs):
+        return ShowContributorViewModifier(request=request, slug=slug, *args, **kwargs)
     
     def get_queryset(self, *args, **kwargs):
         return ShowContributor.permitted.all()
+    
+    def get_extra_context(self, *args, **kwargs):
+        return {'title': 'DJS & Shows'}
 
 showcontributor_detail =  ShowContributorDetail()
 
 class ShowContributorContentDetail(GenericObjectDetail):
-    def get_template_name(self):
+    def get_template_name(self, *args, **kwargs):
         return 'show/showcontributor_content_detail.html'
     
     def get_contributor(self, slug):
         return self.get_queryset().get(slug=slug).owner.modelbase_set.get(class_name='ShowContributor')
 
-    def get_pagemenu(self, request, queryset, slug, *args, **kwargs):
-        return ShowContributorPageMenu(queryset=queryset, request=request, slug=self.get_contributor(slug).slug)
+    def get_view_modifier(self, request, slug, *args, **kwargs):
+        return ShowContributorViewModifier(request=request, slug=self.get_contributor(slug).slug, *args, **kwargs)
     
     def get_queryset(self, *args, **kwargs):
         return ModelBase.permitted.all()
     
-    def get_extra_context(self, slug, *args, **kwargs):
-        extra_context = super(ShowContributorContentDetail, self).get_extra_context(*args, **kwargs)
-        added_context = {
+    def get_extra_context(self, *args, **kwargs):
+        slug=kwargs['slug']
+        return {
             'title': 'DJS & Shows',
             'contributor': self.get_contributor(slug),
         }
-        if extra_context:
-            extra_context.update(
-                added_context,
-            )
-        else:
-            extra_context = added_context
-
-        return extra_context
     
 showcontributor_content_detail =  ShowContributorContentDetail()
 
 class ShowContributorAppearanceList(GenericObjectList):
-    def get_extra_context(self, slug, *args, **kwargs):
-        extra_context = super(ShowContributorAppearanceList, self).get_extra_context(*args, **kwargs)
-        added_context = {
+    def get_extra_context(self, *args, **kwargs):
+        slug = kwargs['slug']
+        return {
             'title': 'DJS & Shows',
             'contributor': ShowContributor.permitted.get(slug=slug)
         }
-        if extra_context:
-            extra_context.update(
-                added_context,
-            )
-        else:
-            extra_context = added_context
-
-        return extra_context
    
-    def get_template_name(self):
+    def get_template_name(self, *args, **kwargs):
         return 'show/showcontributor_appearance_list.html'
     
-    def get_url_callable(self):
-        return None
+    def get_view_modifier(self, request, slug, *args, **kwargs):
+        return ShowContributorViewModifier(request=request, slug=slug, *args, **kwargs)
     
-    def get_pagemenu(self, request, queryset, slug, *args, **kwargs):
-        return ShowContributorPageMenu(queryset=queryset, request=request, slug=slug)
-    
-    def get_paginate_by(self):
+    def get_paginate_by(self, *args, **kwargs):
         return 7
 
-    def get_queryset(self, slug):
+    def get_queryset(self, *args, **kwargs):
+        slug = kwargs['slug']
         contributor = ShowContributor.permitted.get(slug=slug)
         if contributor:
             return EntryItem.permitted.by_model(Event).filter(
