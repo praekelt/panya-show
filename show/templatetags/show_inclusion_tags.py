135e3209b3ec490519722a8d4b940ba7b904cf67
shaunsephton <shaunsephton@gmail.com>
2010-07-13 08:58:49 +0200

    render on air

--- show/templatetags/show_inclusion_tags.py
+++ show/templatetags/show_inclusion_tags.py
@@ -1,5 +1,7 @@
 from django import template
 
+from show.models import Show
+
 register = template.Library()
 
 @register.inclusion_tag('show/inclusion_tags/radioshow_entryitem_listing.html', takes_context=True)
@@ -9,7 +11,16 @@ def radioshow_entryitem_listing(context, object_list):
 
 @register.inclusion_tag('show/inclusion_tags/showcontributor_header.html')
 def showcontributor_header(obj):
-    return {'object': obj}
+    from cal.models import Entry
+    # get entries for the shows the contributor contributes to.
+    shows = Show.objects.filter(contributor=obj)
+    modelbase_objs = [show.modelbase_obj for show in shows]
+    entries = Entry.objects.filter(content__in=modelbase_objs)
+
+    return {
+        'entries': entries,
+        'object': obj
+    }
 
 @register.inclusion_tag('show/inclusion_tags/showcontributor_detail.html')
 def showcontributor_detail(obj):
