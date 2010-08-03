fc13af7d1d49eceb449d6a16211aa4b2333012d1
shaunsephton <shaunsephton@gmail.com>
2010-08-03 12:35:56 +0200

    remed dep links. switched to django

--- setup.py
+++ setup.py
@@ -10,9 +10,6 @@ setup(
     license='BSD',
     url='http://github.com/praekelt/panya-show',
     packages = find_packages(),
-    dependency_links = [
-        'https://github.com/praekelt/panya/tarball/master#egg=panya',
-    ],
     install_requires = [
         'panya',
     ],
@@ -22,7 +19,7 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Development Status :: 4 - Beta",
         "Operating System :: OS Independent",
-        "Framework :: Panya",
+        "Framework :: Django",
         "Intended Audience :: Developers",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
