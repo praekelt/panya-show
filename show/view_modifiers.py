afe5fc41570f749c76f8ed4ed84d98876cbe3130
qoda <jonathan@blu-marble.co.za>
2010-08-19 13:51:29 +0200

    modified to include contributor contact view modifier

--- show/view_modifiers.py
+++ show/view_modifiers.py
@@ -7,7 +7,7 @@ class ShowContributorViewModifier(ViewModifier):
         self.items = [
             URLPatternItem(request, title="Blog", path=reverse('showcontributor_content_list', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_content_list', 'showcontributor_content_detail',], default=False),
             URLPatternItem(request, title="Profile", path=reverse('showcontributor_detail', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_detail',], default=False),
-            #URLPatternItem(request, title="Contact", path=reverse('showcontributor_detail', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_detail',], default=False),
+            URLPatternItem(request, title="Contact", path=reverse('showcontributor_contact', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_contact',], default=False),
             URLPatternItem(request, title="Appearances", path=reverse('showcontributor_appearance_list', kwargs={'slug': slug}), matching_pattern_names=['showcontributor_appearance_list',], default=False),
         ]
         super(ShowContributorViewModifier, self).__init__(request)
